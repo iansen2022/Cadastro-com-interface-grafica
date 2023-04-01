e85203c8679fae00cb844fef32fe6e08bc1f4732
Samuel Iansen <iansen2022@gmail.com>
2023-04-01 20:53:39 -0300

    interface gráfica

---
+++ ViewPrimeiraTela.java
@@ -0,0 +1,73 @@
+package View;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.EventQueue;
+import java.awt.EventQueue;
+import java.awt.*;
+
+public class ViewPrimeiraTela extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtNome;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ViewPrimeiraTela frame = new ViewPrimeiraTela();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ViewPrimeiraTela() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Ação");
+		btnNewButton.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				JOptionPane.showMessageDialog(null, "Nome: "+txtNome.getText());
+			}
+		});
+		btnNewButton.setBounds(118, 187, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		txtNome = new JTextField();
+		txtNome.setBounds(118, 70, 214, 20);
+		contentPane.add(txtNome);
+		txtNome.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Nome");
+		lblNewLabel.setBounds(118, 31, 89, 14);
+		contentPane.add(lblNewLabel);
+	}
+
+}
